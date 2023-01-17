f3976c72303b946f6217db07949ce8dea9928bd3
hoangthaiduong2001 <19521407@gm.uit.edu.vn>
2023-01-18 00:26:26 +0700

    v4

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,59 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useSelector } from 'react-redux'
+import { GetArrSlider } from '../ultis/function'
 
 
 const Slider = () => {
+
   const { banner } = useSelector(state => state.app)
+  useEffect(() => {
+    const sliderEls = document.getElementsByClassName('slider-item')
+    let min = 0;
+    let max = 2;
+    const intervalId = setInterval(() =>{
+      const list = GetArrSlider(min, max, sliderEls.length -1)
+      for(let i = 0; i < sliderEls.length; i++){
+
+        sliderEls[i].classList.remove('animate-slide-right', 'order-last', 'z-20')
+        sliderEls[i].classList.remove('animate-slide-left', 'order-first', 'z-10')
+        sliderEls[i].classList.remove('animate-slide-left2', 'order-2', 'z-10')
+        if(list.some(item => item === i)){
+          sliderEls[i].style.cssText = `display: block`
+        } else{
+          sliderEls[i].style.cssText = `display: none`
+        }
+      }
+      
+      list.forEach(item => {
+        if ( item === max) {
+          sliderEls[item].classList.add('animate-slide-right', 'order-last', 'z-20')
+        } else if( item === min) {
+          sliderEls[item].classList.add('animate-slide-left', 'order-first', 'z-10')
+        } else {
+          sliderEls[item].classList.add('animate-slide-left2', 'order-2', 'z-10')
+        }
+      })
+
+      min = (min === sliderEls.length - 1) ? 0 : min + 1
+      max = (max === sliderEls.length - 1) ? 0 : max + 1
+      console.log(list)
+    }, 2000)
+
+    return () => {
+      intervalId && clearInterval(intervalId)
+    }
+  }, [])
+
   return (
-    <div className='flex flex-col'>
-        {banner?.map(item => (
-            <img
-            src={item.banner}
-            className='flex-1 object-contain' 
-            />
-        ))}
+    <div className='w-full overflow-hidden px-[59px]'>
+      <div className='flex w-full gap-8 pt-8'>
+          {banner?.map((item, index) => (
+              <img
+              src={item.banner}
+              className={`slider-item flex-1 object-contain w-[30%] rounded-lg ${index <= 2 ? 'block' : 'hidden'}`}
+              />
+          ))}
+      </div>
     </div>
   )
 }
---
+++ src/ultis/function.js
@@ -0,0 +1,14 @@
+export const GetArrSlider = (start, end, number) => {
+    let output = []
+    const limit = start > end ? number : end
+    for (let i = start; i <= limit; i++){
+        output.push(i);
+    }
+    if(start > end){
+        for( let i = 0; i <= end; i++){
+            output.push(i)
+        }
+    }
+    return output
+}
+
