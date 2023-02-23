ed39e7851cc528abd703ffb304c65a252054926f
hoangthaiduong2001 <19521407@gm.uit.edu.vn>
2023-02-23 14:40:48 +0700

    v19-2

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,8 +1,18 @@
 import React, { memo } from 'react'
+import moment from 'moment'
+import 'moment/locale/vi'
+import { NewRelease } from '.';
 
-const SongItem = () => {
+const SongItem = ({ thumbnail, title, artists, releaseDate }) => {
   return (
-    <div>SongItem</div>
+    <div className='w-[30%] flex flex-auto p-[10px] gap-[10px] hover:bg-main-200 rounded-md cursor-pointer'>
+      <img src={thumbnail} className='w-[60px] h-[60px] object-cover rounded-md'/>
+      <div className='flex flex-col'>
+        <span className='text-sm font-semibold'>{title}</span>
+        <span className='text-xs text-gray-600'>{artists}</span>
+        <span className='text-xs text-gray-600'>{moment(releaseDate * 1000).fromNow()}</span>
+      </div>
+    </div>
   )
 }
 
